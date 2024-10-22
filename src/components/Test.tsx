c4c9e493ac79393c5c22e1c6c79919965f86445a
wmorgan <william.morgan@pnmac.com>
2024-10-22 13:19:13 -0700

    function

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 export const Test = () => {
+  const [data, setData] = React.useState({ test: "" });
+
   React.useEffect(() => {
     const getTest = async () => {
       const testData = await fetch("/api/test");
-      const testJson = await testData.json();
-      console.log(testJson);
+      const response = await testData.json();
+      setData(response);
     };
 
     getTest();
   }, []);
 
-  return <p>hello</p>;
+  return <p>hello {data.test}</p>;
 };
